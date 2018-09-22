d40b31d465c73a917621a853a66d1c649670ada8
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-22 10:18:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -689,20 +689,6 @@ if (message.content.startsWith("ban")) {
 };
 });
 
-client.on('guildMemberRemove', member => {
-    var embed = new Discord.RichEmbed()
-    .setAuthor(member.user.username, member.user.avatarURL)
-    .setThumbnail(member.user.avatarURL)
-    .setTitle(`خرج عضو`)
-    .setDescription(`الى اللقاء...`)
-    .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-    .setColor('RED')
-    .setFooter(`On.Rz Bot .`, '')
-
-var channel =member.guild.channels.find('name', 'remove-members')
-if (!channel) return;
-channel.send({embed : embed});
-});
 
 client.login(process.env.BOT_TOKEN);
 
