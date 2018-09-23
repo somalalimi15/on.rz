bd32e685534582da2209cdc17a41f9a51cb8de9f
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-23 12:32:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -300,16 +300,44 @@ client.on("message", message => {
   } else return;
 }})
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  }); 
-});
+client.on('message', message =>{
+  let args = message.content.split(" ").slice(1).join(" ");
+  if(message.content.startsWith('الوان')) {
+  const hexString = ['A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  function toHex(str) {
+      var hex = '';
+      for(var i=0;i<str.length;i++) {
+          hex += ''+str.charCodeAt(i).toString(16);
+      }
+      return hex;
+  }
+  let color = args.join(' ');
+  if(message.content.includes('convert')) {
+    let colorStringToConvert = message.content.slice('u!hexcol convert ').split(' ');
+    if(colorStringToConvert.length != 3) {return message.channel.send("You can't convert a string bigger or smaller than 3")} else {
+      color = toHex(colorStringToConvert[2])
+      return message.channel.send("", {
+        embed: new Discord.RichEmbed()
+          .setTitle("Color checker")
+          .setDescription("Here is your color!")
+          .setColor(`#${color}`)
+          .setFooter("Requested by: " + message.author.tag)
+      });
+    }
+  }
+  if(hexString.some(word => message.content.includes(word)) ) {
+    if(color.length != 6) return message.channel.send('This is not a valid color!')
+    if(color.length === 6) {
+      return message.channel.send("", {
+        embed: new Discord.RichEmbed()
+          .setTitle("Color checker")
+          .setDescription("Here is your color!")
+          .setColor(`#${color}`)
+          .setFooter("Requested by: " + message.author.tag)
+      });
+    }
+  } else return message.channel.send("You can't use non hex values!")
+}})
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
