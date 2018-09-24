3c5259a02f3d4c899ad878941351090df7b5cd57
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-24 09:21:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -200,53 +200,7 @@ function datediff(first, second) {
     return Math.round((second-first)/(1000*60*60*24));
 };
 
-client.on('message', function(message) {
-  if (message.content.startsWith(prefix + 'info')) {
-  message.delete;
-  message.channel.send("", {
-      embed: {
-          color : 0xE15306,
-  author :{name: message.author.name},
-          description : "",
-          title : "Server Info",
-          fields : [
-              {
-   name : '**Server Name**',
-   value : message.guild.name,
-   inline : true
-            },{
-   name : '**Owner**',
-   value : message.guild.owner.user.tag,
-   inline : true
- },{
-    name :'**Verification Level**',
-    value : message.guild.verificationLevel,
-    inline : true
- },{
-    name : '**Region**',
-    value : message.guild.region,
-    inline : true
- },{
-   name : '**Members Count**',
-   value : message.guild.memberCount,
-   inline : true
- },{
-   name : '**Humans**',
-   value : message.guild.members.filter(m => m.user).size,
-   inline : true
- },{
-   name : '**Bots**',
-   value : message.guild.members.filter(m => m.user.bot).size,
-   inline : true
- }],
-},
-footer : {
-   text : 'RANDOM'
-          }
-    });
-}
 
-	
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`On.Rz | Owner : SoM .`,"http://twitch.tv/S-F")
